b21a0e63101fe55165805e8b67d44bb79cf088a0
Miguel Perez <36670243+miguelperez1@users.noreply.github.com>
2021-12-12 01:23:33 -0600

    = updates

--- tools_core/asset_library/asset_browser/asset_browser_standalone_ui.py
+++ tools_core/asset_library/asset_browser/asset_browser_standalone_ui.py
@@ -3,6 +3,7 @@ from functools import partial
 
 from PySide2 import QtCore
 from PySide2 import QtWidgets
+from PySide2 import QtGui
 
 from tools_core.asset_library.asset_browser import AssetBrowserWidget
 from tools_core.asset_library import library_manager as lm
@@ -110,6 +111,25 @@ class AssetBrowser(QtWidgets.QWidget):
 
 def main():
     app = QtWidgets.QApplication(sys.argv)
+
+    app.setStyle(QtWidgets.QStyleFactory.create("fusion"))
+
+    dark_palette = QtGui.QPalette()
+    dark_palette.setColor(QtGui.QPalette.Window, QtGui.QColor(45, 45, 45))
+    dark_palette.setColor(QtGui.QPalette.WindowText, QtGui.QColor(208, 208, 208))
+    dark_palette.setColor(QtGui.QPalette.Base, QtGui.QColor(25, 25, 25))
+    dark_palette.setColor(QtGui.QPalette.AlternateBase, QtGui.QColor(208, 208, 208))
+    dark_palette.setColor(QtGui.QPalette.ToolTipBase, QtGui.QColor(208, 208, 208))
+    dark_palette.setColor(QtGui.QPalette.ToolTipBase, QtGui.QColor(208, 208, 208))
+    dark_palette.setColor(QtGui.QPalette.Text, QtGui.QColor(208, 208, 208))
+    dark_palette.setColor(QtGui.QPalette.Button, QtGui.QColor(45, 45, 48))
+    dark_palette.setColor(QtGui.QPalette.ButtonText, QtGui.QColor(208, 208, 208))
+    dark_palette.setColor(QtGui.QPalette.BrightText, QtCore.Qt.red)
+    dark_palette.setColor(QtGui.QPalette.Link, QtGui.QColor(42, 130, 218))
+    dark_palette.setColor(QtGui.QPalette.Highlight, QtGui.QColor(42, 130, 218))
+    dark_palette.setColor(QtGui.QPalette.Highlight, QtCore.Qt.black)
+    app.setPalette(dark_palette)
+
     browser = AssetBrowser()
     browser.show()
     sys.exit(app.exec_())
